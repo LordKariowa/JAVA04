b8a6775d4914b44a5ea2b752f5b9f7c79ac39fce
MARCUS VINICIUS DE SOUZA <MARCUS.VINICIUS@csi.aneas.msft>
2019-02-26 20:19:59 -0300

    JAVA04

---
+++ src/JAVA04.java
@@ -0,0 +1,24 @@
+
+public class JAVA04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("testando condicionais");
+		int idade = 18;
+		int qtdPessoas = 3;
+		
+		if (idade >= 18 || qtdPessoas >= 3) {
+		System.out.println("Bem vindos !");
+		}
+		else {
+			System.out.println("Rala daqui se não chamo tua mãe !");
+		}
+
+	
+	   }
+		
+		
+	}
+
+
---
+++ src/TesteWhile.java
@@ -0,0 +1,19 @@
+
+public class TesteWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 1;
+		while (i <= 5) {
+		System.out.println("oi");
+		
+		
+		
+		
+		i++;;
+		}
+		
+	}
+
+}
